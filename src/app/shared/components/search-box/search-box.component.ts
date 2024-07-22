bef61b8c32eb4b64fca455519e698755d58e00cd
jere <jere77lagos@gmail.com>
2024-07-22 19:20:53 +0200

    implement output

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -8,5 +8,13 @@ export class SearchBoxComponent {
 
   @Input()
   public placeholder: string = ''
+
+  @Output() onValue = new EventEmitter<string>()
+
+  emitValue( value: string ): void {
+    this.onValue.emit(value)
+  }
+
+
 
 }
